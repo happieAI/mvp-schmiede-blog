117f4e5b4b6477286d1c2d637089a1f3a45491ef
Anky <ann-cathrin.konrad@capsloq.de>
2023-01-12 19:29:30 +0100

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,14 +6,14 @@ export default function Footer() {
   return (
     <footer>
       <div className="mt-16 flex flex-col items-center">
-        <div className="mb-3 flex space-x-4">
-          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />
+        {/* <div className="mb-3 flex space-x-4"> */}
+        {/* <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />
           <SocialIcon kind="github" href={siteMetadata.github} size="6" />
           <SocialIcon kind="facebook" href={siteMetadata.facebook} size="6" />
           <SocialIcon kind="youtube" href={siteMetadata.youtube} size="6" />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
-          <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
-        </div>
+          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" /> */}
+
+        {/* </div> */}
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
           <div>{` • `}</div>
@@ -25,6 +25,14 @@ export default function Footer() {
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
             Tailwind Nextjs Theme
           </Link>
+        </div>
+        <div className="grid grid-cols-2">
+          <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+            <Link href="https://landladen.de/pages/impressum">Impressum</Link>
+          </div>
+          <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+            <Link href="https://landladen.de/pages/datenschutzerklarung">Datenschutzerklärung</Link>
+          </div>
         </div>
       </div>
     </footer>
